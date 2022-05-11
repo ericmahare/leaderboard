2ac535cc48c9fb09892dca56648b12f6d3da6872
ericmahare <erikmahare@gmail.com>
2022-05-11 17:07:39 +0300

    add post and read api functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,67 @@
 import './main.css';
-import addUi from './modules/ui.js';
+import App from './modules/app.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  addUi();
+const form = document.querySelector('form');
+const refresh = document.querySelector('#refresh');
+
+document.addEventListener('DOMContentLoaded', async () => {
+  App.displayData();
 });
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  App.postData();
+});
+
+refresh.addEventListener('click', (e) => {
+  e.preventDefault();
+  App.displayData();
+});
+
+// const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BT0cx2WqtrqYvdaTYVfQ/scores/';
+
+// async function fetchScores() {
+//   const response = await fetch(baseURL);
+//   return response.json();
+// }
+
+// async function postScore(score) {
+//   const response = await fetch(baseURL, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(score),
+//   });
+//   return response.json();
+// }
+
+// const displayScores = () => {
+//   fetchScores()
+//     .then((data) => {
+//       let html = '';
+//       data.result.forEach((item) => {
+//         html += `<tr><td>${item.user} : ${item.score}</td></tr>`;
+//       });
+//       document.querySelector('tbody').innerHTML = html;
+//     })
+//     .catch((error) => {
+//       document.querySelector('tbody').innerHTML = error;
+//     });
+// };
+
+// const GetScoreFormValues = () => {
+//   const name = document.querySelector('.userName').value;
+//   const score = document.querySelector('.userScore').value;
+//   postScore({ user: name, score })
+//     .then(() => {
+//       displayScores();
+//       document.querySelector('.userName').value = '';
+//       document.querySelector('.userScore').value = '';
+//     })
+//     .catch((error) => {
+//       document.querySelector('.result').textContent = error;
+//     });
+// };
+
+// module.exports = { GetScoreFormValues, displayScores };
